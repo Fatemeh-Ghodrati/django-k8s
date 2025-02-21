22685ad73ec71b8cee88467dee9992a778f6befc
Fatemeh Ghodrati <ghodratifatemeh82@gmail.com>
2025-02-21 23:27:57 +0330

    Probe updates

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('users/new_user/', views.USerCreateView.as_view(), name='user-create'),
     path('users/<int:user_id>/', views.UserDetailView.as_view(), name='user-detail'),
+    path('liveness/', views.LivenessProbeView.as_view(), name='liveness_probe'),
+    path('readiness/', views.ReadinessProbeView.as_view(), name='readiness_probe'),
 ]
